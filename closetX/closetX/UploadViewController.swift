8630cb2543816b90316ce50907fcaa0bff511c5b
Lauren Spatz <laurens914@gmail.com>
2016-04-20 14:03:26 -0700

    scroll view, upload VC

--- closetX/closetX/UploadViewController.swift
+++ closetX/closetX/UploadViewController.swift
@@ -7,16 +7,66 @@
 //
 
 import UIKit
+import Photos
 
 class UploadViewController: UIViewController
 {
+    
+    lazy var imagePicker = UIImagePickerController()
+    
+    
 
+    @IBAction func camera(sender: AnyObject)
+    {
+       self.showImagePicker(.Camera)
+    
+    }
+    @IBAction func photoLibraryView(sender: AnyObject)
+    {
+       let libraryAuth = PHPhotoLibrary.authorizationStatus()
+        if libraryAuth == PHAuthorizationStatus.Denied
+        {
+            NSOperationQueue.mainQueue().addOperationWithBlock({ 
+                 self.presentDeniedAccessVC()
+            })
+        }
+        else {
+            NSOperationQueue.mainQueue().addOperationWithBlock({ 
+                self.showImagePicker(.PhotoLibrary)
+            })
+        }
+
+    }
+    
+
+    func presentDeniedAccessVC()
+    {
+       let alertView = UIAlertController(title: "Error", message: "PLEASE ALLOW ACCESS TO PHOTOS IN SETTINGS", preferredStyle: .Alert)
+       let ok = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
+        alertView.addAction(ok)
+        self.presentViewController(alertView, animated: true, completion: nil)
+    }
+    
+    
+    func showImagePicker(source: UIImagePickerControllerSourceType)
+    {
+        self.imagePicker.sourceType = source
+        self.presentViewController(self.imagePicker, animated: true, completion: nil)
+    }
+ 
+    
+    @IBAction func dismissVC(sender: AnyObject)
+    {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
     override func viewDidLoad()
     {
         super.viewDidLoad()
 
         
     }
+    
+    
 
     override func didReceiveMemoryWarning()
     {
