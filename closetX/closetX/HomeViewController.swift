88445f8a66976ed0a41f343d6a9eca8a61e619d3
Lacey Vu <laceylin2010@gmail.com>
2016-04-25 13:08:08 -0700

    users successfully logged in

--- closetX/closetX/HomeViewController.swift
+++ closetX/closetX/HomeViewController.swift
@@ -8,9 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController
+class HomeViewController: UIViewController
 {
 
+    
+    @IBOutlet weak var loginPasswordTextField: UITextField!
+    @IBOutlet weak var loginUsernameTextField: UITextField!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -22,7 +26,45 @@ class ViewController: UIViewController
         super.didReceiveMemoryWarning()
         
     }
-
-
+    
+    func loginToApp()
+    {
+        let urlPath = "https://closetx.herokuapp.com/api/login"
+        let url = NSURL(string: urlPath)
+        let request = NSMutableURLRequest(URL: url!)
+        let username = self.loginUsernameTextField.text
+        let password = self.loginPasswordTextField.text
+        let loginString = NSString(format: "%@:%@", username!, password!) //come back here
+        let loginData = loginString.dataUsingEncoding(NSUTF8StringEncoding)
+        let base64LoginString = loginData!.base64EncodedStringWithOptions(NSDataBase64EncodingOptions())
+        request.HTTPMethod = "GET"
+        request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
+        print(base64LoginString)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+          data, response, error in
+            if error != nil {
+                return
+            } else {
+                print(request)
+            }
+            
+            let response = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            print(response)
+            do {
+                if let convertingJSON = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as? NSDictionary {
+                    print(convertingJSON)
+                }
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
+    
+    @IBAction func loginAction(sender: UIButton)
+    {
+        loginToApp()
+    }
+    
 }
-
